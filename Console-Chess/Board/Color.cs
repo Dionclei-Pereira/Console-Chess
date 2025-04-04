52e0da2bff1385d8971632fe30e6abb0653660c8
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-04-04 16:25:12 -0300

    Colors enum

---
+++ Console-Chess/Board/Color.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Chess.Board {
+    internal enum Color {
+        RED,
+        GREEN,
+        BLUE,
+        BLACK,
+        WHITE,
+        YELLOW,
+        ORANGE
+    }
+}
